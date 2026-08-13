f55faafd6c3713d0562f1ae3b389a0b01fd74ea4
FIA-FTC Coder <fiaftccoder@gmail.com>
2026-08-13 19:23:09 -0400

    Added moveForwardTuff Fuction Distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonStarter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonStarter.java
@@ -67,7 +67,7 @@ public class AutonStarter extends LinearOpMode {
         rightIntakeServoTuff.setPower(intakeServoPowerTuff);
 
         // Drive forward for 1 sec
-        moveForwardTuff(drivePowerTuff, 1000);
+        moveForwardTuff(drivePowerTuff, 1151);
 
         // Shut it down
         intakeTuff.setPower(0);

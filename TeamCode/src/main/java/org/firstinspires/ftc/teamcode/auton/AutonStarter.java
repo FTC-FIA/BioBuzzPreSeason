9946a1cad501817bfa9af3c9b7293eb152d009c2
FIA-nova-1 <fiaftccoder@gmail.com>
2026-08-20 18:56:20 -0400

    Added the moveForward func.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonStarter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonStarter.java
@@ -69,6 +69,7 @@ public class AutonStarter extends LinearOpMode {
         moveForward(drivePower, 1000);
 
         // Shut it down
+
         intake.setPower(0);
         leftIntakeServo.setPower(0);
         rightIntakeServo.setPower(0);

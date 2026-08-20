66308749f1eeb520707210b6739d1627ee90a014
larsnewman-taylor <quillinity@gmail.com>
2026-08-20 19:22:14 -0400

    wayfinder chall

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonStarter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonStarter.java
@@ -57,7 +57,7 @@ public class AutonStarter extends LinearOpMode {
         waitForStart();
 
         // Start the auton program
-        double drivePower = 0.8;
+        double drivePower = -0.8;
         double intakePower = 1.0;
         double intakeServoPower = 1.0;
 
@@ -66,14 +66,14 @@ public class AutonStarter extends LinearOpMode {
         leftIntakeServo.setPower(intakeServoPower);
         rightIntakeServo.setPower(intakeServoPower);
 
-        moveForward(-drivePower, 500);
+        moveForward(drivePower, 500);
         turn(true, 1, 1000);
         for(int i=0;i<3;i++) {
-            moveForward(-drivePower, 1000);
+            moveForward(drivePower, 1000);
             turn(true, 1, 1000);
         }
         turn(false, 1, 1000);
-        moveForward(drivePower, 1000);
+        moveForward(-drivePower, 1000);
 
 
 
